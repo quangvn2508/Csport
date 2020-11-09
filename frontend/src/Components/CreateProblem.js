bfbc9f1a92c3d9a2fbc0e104b21c03409974af57
Vinh Quang Nguyen <quangvn2508@gmail.com>
2020-11-09 22:15:13 +0000

    Statement tab on create problem page

--- frontend/src/Components/CreateProblem.js
+++ frontend/src/Components/CreateProblem.js
@@ -1,9 +1,80 @@
 import React from 'react';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
 
-class CreateProblem extends React.Component {
+class ProblemStatement extends React.Component {
+    state = {
+        title: '',
+        statement: ''
+    }
+
+    editorOptions = {toolbar: [
+        "bold", "italic", "strikethrough", "|",
+        "heading-1", "heading-2", "heading-3", "|",
+        "code", "quote", "unordered-list", "ordered-list", "horizontal-rule", "table", "|",
+        "link",
+        // {
+        //     name: "custom-image",
+        //     action: ,
+        //     className: "fa fa-picture-o",
+        //     title: "Insert image",
+        // },
+         "|",
+        "preview"
+    ]};
+
+    
+    validateForm = (event) => {return true};
+    updateTitle = (event) => {this.setState({title: event.target.value});};
+    updateStatement = (value) => {this.setState({statement: value});};
+
+    submitNewProblem = (event) => {
+        event.preventDefault();
+
+        console.log("title: " + this.state.title + "\nstatement: " + this.state.statement);
+    }
+
     render() {
         return (
-            <div>This is create problem page</div>
+            <div>
+                <form onSubmit={this.submitNewProblem}>
+                    <input type="text" value={this.state.title} onChange={this.updateTitle}/>
+                    <input type="hidden" value={this.state.statement}/>
+                    <div>
+                        <SimpleMDE onChange={this.updateStatement} options={this.editorOptions}/>
+                    </div>
+                    <input type="submit" disabled={!this.validateForm()}/>
+                </form>
+            </div>
+        )
+    }
+}
+
+class ProblemTestcase extends React.Component {
+    render() {
+        return (
+            <div>Test case tab</div>
+        );
+    }
+}
+
+class CreateProblem extends React.Component {
+    state = {
+        key: "statement"
+    }
+
+    render() {
+        return (
+            <Tabs id="controlled-tab-example" activeKey={this.state.key} onSelect={(k) => this.setState({key: k})}>
+                <Tab eventKey="statement" title="Problem Statement">
+                    <ProblemStatement/>
+                </Tab>
+                <Tab eventKey="testcase" title="Testcase">
+                    <ProblemTestcase/>
+                </Tab>
+            </Tabs>
         );
     }
 }
--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from './Components/Home';
-import Problem from './Components/Problem';
+// import Problem from './Components/Problem';
 import CreateProblem from './Components/CreateProblem';
 import Invalid from './Components/Invalid';
 
@@ -11,11 +11,11 @@ class Routes extends React.Component {
         return (
             <Switch>
                 <Route exact path="/createProblem" component={CreateProblem}/>
-                <Route exact path="/problem" component={Problem}/>
-                <Route exact path="/problems" component={Home}/>
-                <Route exact path="/rank" component={Home}/>
-                <Route exact path="/login" component={Home}/>
-                <Route exact path="/profile" component={Home}/>
+                {/* <Route exact path="/problem" component={Problem}/> */}
+                {/* <Route exact path="/problems" component={Home}/> */}
+                {/* <Route exact path="/rank" component={Home}/> */}
+                {/* <Route exact path="/login" component={Home}/> */}
+                {/* <Route exact path="/profile" component={Home}/> */}
                 <Route exact path={["/", "/home"]} component={Home}/>
                 <Route component={Invalid}/>
                 
