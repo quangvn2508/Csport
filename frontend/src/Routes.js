79d75e2f9220fd4afb163899d538b462fdf8eaf6
Vinh Quang Nguyen <quangvn2508@gmail.com>
2020-11-09 16:33:31 +0000

    add 404 page, include new routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from './Components/Home';
 import Problem from './Components/Problem';
 import CreateProblem from './Components/CreateProblem';
+import Invalid from './Components/Invalid';
 
 class Routes extends React.Component {
     render() {
@@ -11,10 +12,15 @@ class Routes extends React.Component {
             <Switch>
                 <Route exact path="/createProblem" component={CreateProblem}/>
                 <Route exact path="/problem" component={Problem}/>
-                <Route exact path="/" component={Home}/>
-                
+                <Route exact path="/problems" component={Home}/>
+                <Route exact path="/rank" component={Home}/>
+                <Route exact path="/login" component={Home}/>
+                <Route exact path="/profile" component={Home}/>
+                <Route exact path={["/", "/home"]} component={Home}/>
+                <Route component={Invalid}/>
                 
                 {/* https://ui.dev/react-router-v4-protected-routes-authentication/ */}
+                {/* https://stackoverflow.com/questions/43164554/how-to-implement-authenticated-routes-in-react-router-4 */}
             </Switch>
         );
     }
