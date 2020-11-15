fbf22a45825530ee9014d2bf93b4a4543591e02b
Vinh Quang Nguyen <quangvn2508@gmail.com>
2020-11-15 21:09:22 +0000

    change navbar color

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -2,17 +2,17 @@ import {Navbar, Nav} from 'react-bootstrap';
 
 function Navigation() {
     return (
-        <Navbar bg="danger" expand="sm">
-        <Navbar.Brand href="#home">Csport</Navbar.Brand>
+        <Navbar bg="light" expand="sm">
+        <Navbar.Brand href="/">Csport</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="#problem">Problems</Nav.Link>
+                <Nav.Link href="/problems">Problems</Nav.Link>
                 <Nav.Link href="#ranking">Ranking</Nav.Link>
             </Nav>
             <Nav>
-                <Nav.Link href="#Hello">Join us</Nav.Link>
-                <Nav.Link href="#Hello">Profile</Nav.Link>
+                <Nav.Link href="#Login">Join us</Nav.Link>
+                <Nav.Link href="#Profil">Profile</Nav.Link>
             </Nav>
         </Navbar.Collapse>
         </Navbar>
