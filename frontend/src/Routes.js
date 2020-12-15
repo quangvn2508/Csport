e71c5e65fb7b760469ae80f17c2ba79380214cb7
Quang <quangvn2508@gmail.com>
2020-12-15 20:29:44 +0000

    remove login route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -7,7 +7,6 @@ import Problems from './Components/Problems';
 import CreateProblem from './Components/CreateProblem';
 import Invalid from './Components/Invalid';
 import Ranking from './Components/Ranking';
-import Login from './Components/Login';
 import SecuredRoute from './Components/SecuredRoute';
 import Profile from './Components/Profile';
 
@@ -19,7 +18,6 @@ class Routes extends React.Component {
                 <Route exact path="/problem/:problemId" component={Problem}/>
                 <Route exact path="/problems" component={Problems}/>
                 <Route exact path="/rank" component={Ranking}/>
-                <Route exact path="/login" component={Login}/>
                 <Route exact path="/profile" component={Profile}/>
                 <Route exact path={["/", "/home"]} component={Home}/>
                 <Route component={Invalid}/>
