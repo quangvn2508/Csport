7a67d6d00f74f5e4452f314c2d9a7eb1d7f85dd3
Quang <quangvn2508@gmail.com>
2020-12-15 20:37:17 +0000

    db controller for user, problem

---
+++ backend/controller.py
@@ -0,0 +1,19 @@
+class UserTable(object):
+    def get():
+        pass
+
+    def add():
+        pass
+
+    def update():
+        pass
+
+class ProblemTable(object):
+    def get():
+        pass
+
+    def add():
+        pass
+
+    def update():
+        pass
