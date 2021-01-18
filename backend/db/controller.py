a689a3680d17272bb3f989b89919999ec0f4c3e3
Quang Nguyen <quangvn2508@gmail.com>
2021-01-18 11:26:30 +0000

    Seperate dao controller and file controller (#12)
    - Add function documentation for file controller function
    - Rename dao controller for consistency

--- backend/db/controller.py
+++ backend/db/controller.py
@@ -1,16 +1,13 @@
-import uuid
-import os
-import zipfile
 from db.dao import UserDao, ProblemDao, SubmissionDao, TestcaseDao
 from db.entities import *
 from Judge.SubmissionResult import SubmissionResult
 from typing import List
+import db.file_util as ftl
 
 UPLOAD_FOLDER = 'uploads/'
-TESTCASE_FOLDER = 'testcases/'
-ALLOWED_EXTENSIONS = {'txt', 'png', 'jpg', 'jpeg', 'gif', 'cpp', 'py'}
 
-def get_user_account(user_id: int) -> dict:
+# user
+def get_user(user_id: int) -> dict:
     user = UserDao.getInstance().get(user_id)
     if user is None:
         return None
@@ -24,37 +21,14 @@ def get_user_id(social_id: str) -> int:
     
     return user.UserId
 
+def create_user(social_id: str) -> int:
+    return UserDao.getInstance().add(social_id)
+
 def registered_social_id(social_id: str) -> bool:
     return UserDao.getInstance().get_with_social_id(social_id) != None
 
-def create_new_account(social_id: str):
-    new_id = UserDao.getInstance().add(social_id)
-
-def file_extension_from_name(filename: str) -> str:
-    # Check if file name contain '.' character
-    if not '.' in filename:
-        return None
-
-    return filename.rsplit('.', 1)[1].lower()
-
-def upload_file(file) -> str:
-    # Get file extension
-    fileExtension = file_extension_from_name(file.filename)
-
-    if not fileExtension in ALLOWED_EXTENSIONS:
-        raise Exception('File type not allowed')
-
-    # TODO Check allowed extension
-
-    # Create new unique name for file
-    filename = str(uuid.uuid4()) + '.' + fileExtension
-
-    file.save(UPLOAD_FOLDER + filename)
-
-    # Return static url
-    return '/api/uploads/' + filename
-
-def create_new_problem(user_id: int, title: str, statement: str) -> int:
+# problem
+def create_problem(user_id: int, title: str, statement: str) -> int:
     return ProblemDao.getInstance().add(user_id, title, statement)
 
 def get_problem(problem_id: int):
@@ -64,27 +38,14 @@ def get_problem(problem_id: int):
     
     return problem.to_json()
 
-def get_list_problems() -> List:
+def get_problems_all() -> List:
     return [problem.to_json() for problem in ProblemDao.getInstance().getAll()]
 
-def add_submission(user_id: int, language: str, code_url: str, problem_id: int) -> int:
-    path_to_code_file = UPLOAD_FOLDER + code_url.rsplit('/',1)[1]
+# submission
+
+def create_submission(user_id: int, language: str, code_url: str, problem_id: int) -> int:
+    path_to_code_file = ftl.file_url_to_file_path(code_url)
     return SubmissionDao.getInstance().add(user_id, language, path_to_code_file, problem_id)
-
-def update_verdict(submission_id: int, status: bool, log: str):
-    SubmissionDao.getInstance().update(submission_id, status, log)
-
-def problem_testcase_directory(problem_id: int) -> str:
-    return TESTCASE_FOLDER + 'problem_' + str(problem_id) + '/'
-
-def extract_testcase(zip_url: str, problem_id: int):
-    path_to_zip_file = UPLOAD_FOLDER + zip_url.rsplit('/',1)[1]
-    directory_to_extract_to = problem_testcase_directory(problem_id)
-    
-    os.mkdir(directory_to_extract_to)
-
-    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
-        zip_ref.extractall(directory_to_extract_to)
 
 def get_submission(submission_id: int):
     submission = SubmissionDao.getInstance().get(submission_id)
@@ -95,6 +56,10 @@ def get_submission(submission_id: int):
 
     return submission_json
 
+def update_submission(submission_id: int, status: bool, log: str):
+    SubmissionDao.getInstance().update(submission_id, status, log)
+
+# judge
 def finish_judge_submission(submission_id: int, result: SubmissionResult):
     for testcase in result.testcase_verdict:
         TestcaseDao.getInstance().add(submission_id, testcase.test_no, testcase.duration, testcase.verdict.value)
---
+++ backend/db/file_util.py
@@ -0,0 +1,59 @@
+import uuid
+import os
+import zipfile
+
+UPLOAD_FOLDER = 'uploads/'
+TESTCASE_FOLDER = 'testcases/'
+ALLOWED_EXTENSIONS = {'txt', 'png', 'jpg', 'jpeg', 'gif', 'cpp', 'py'}
+
+def file_url_to_file_path(file_url: str):
+    """
+        Get file local location using file url location
+    """
+    return UPLOAD_FOLDER + file_url.rsplit('/',1)[1]
+
+def problem_testcase_directory(problem_id: int) -> str:
+    """
+        return testcase directory corresponding to problem id
+    """
+    return TESTCASE_FOLDER + 'problem_' + str(problem_id) + '/'
+
+def file_extension_from_name(filename: str) -> str:
+    """
+        Get file extension from file name
+    """
+    # Check if file name contain '.' character
+    if not '.' in filename:
+        return None
+
+    return filename.rsplit('.', 1)[1].lower()
+
+def upload_file(file) -> str:
+    """
+        Save file to local directory and return the global url for file
+    """
+    # Get file extension
+    fileExtension = file_extension_from_name(file.filename)
+
+    if not fileExtension in ALLOWED_EXTENSIONS:
+        raise Exception('File type not allowed')
+
+    # Create new unique name for file
+    filename = str(uuid.uuid4()) + '.' + fileExtension
+
+    file.save(UPLOAD_FOLDER + filename)
+
+    # Return static url
+    return '/api/uploads/' + filename
+
+def extract_testcase(zip_url: str, problem_id: int):
+    """
+        Unzip zip file provided by zip_url
+    """
+    path_to_zip_file = file_url_to_file_path(zip_url)
+    directory_to_extract_to = problem_testcase_directory(problem_id)
+    
+    os.mkdir(directory_to_extract_to)
+
+    with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
+        zip_ref.extractall(directory_to_extract_to)
